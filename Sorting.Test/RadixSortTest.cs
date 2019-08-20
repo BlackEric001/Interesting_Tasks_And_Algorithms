057c91fa26d3dd0cf5f387a73b93efbcaa1c195a
BlackEric001 <ericblack@mail.ru>
2019-08-20 20:29:09 +0300

    Radix sort с поддержкой отрицательных чисел

--- Sorting.Test/RadixSortTest.cs
+++ Sorting.Test/RadixSortTest.cs
@@ -11,26 +11,26 @@ namespace Sorting.Test
         [Fact]
         public void TestNull()
         {
-            var res = RadixSort.Sort(null);
+            var res = RadixSort.SortL(null);
 
             Assert.Null(res);
         }
 
         [Theory]
-        [InlineData(new int[] { })]
-        [InlineData(new int[] { 1 })]
-        [InlineData(new int[] { 1, 2, 3 })]
-        [InlineData(new int[] { 1, 1, 1 })]
-        [InlineData(new int[] { 3, 2, 3 })]
-        [InlineData(new int[] { 3, 2, 1 })]
-        [InlineData(new int[] { 1, 2, 3, 1 })]
-        [InlineData(new int[] { -1, 123, 5, 7, 4000, 8, 567, 987, 311, 900, 0, -1 })]
-        [InlineData(new int[] { -1, 123, 5, 7, 4000, 8, 567, 987, 311, 900, 0, -1, 311 })]
-        [InlineData(new int[] { 1, 5, 1, 2, 10, 6, 9, 8, 3, 7, 4 })]
-        [InlineData(new int[] { 4, 4, 4, 4, 4, 4, 4, 3, 4, 4, 4, 4, 4, 4, 4, 4 })]
-        public void Test(int[] arr)
+        [InlineData(new long[] { })]
+        [InlineData(new long[] { 1 })]
+        [InlineData(new long[] { long.MaxValue, 1, 2, 3, long.MinValue })]
+        [InlineData(new long[] { 1, 1, 1 })]
+        [InlineData(new long[] { 3, 2, int.MaxValue, 3 })]
+        [InlineData(new long[] { 3, 2, 1 })]
+        [InlineData(new long[] { 1, 2, 3, 1 })]
+        [InlineData(new long[] { -1, 123, 5, 7, 4000, 8, 567, 987, 311, 900, 0, -1 })]
+        [InlineData(new long[] { -1, 123, 5, 7, 4000, 8, 567, 987, 311, 900, 0, -1, 311 })]
+        [InlineData(new long[] { 1, 5, 1, 2, 10, 6, 9, 8, 3, 7, 4 })]
+        [InlineData(new long[] { 4, 4, 4, 4, 4, 4, 4, 3, 4, 4, 4, 4, 4, 4, 4, 4 })]
+        public void Test(long[] arr)
         {
-            var res = RadixSort.Sort((int[])arr.Clone());
+            var res = RadixSort.SortL((long[])arr.Clone());
 
             Assert.Equal(arr.OrderBy(x => x), res);
         }
--- Sorting/RadixSort.cs
+++ Sorting/RadixSort.cs
@@ -1,83 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Sorting
 {
     public class RadixSort
     {
-        /// <summary>
-        /// A utility function to get maximum value in arr[] 
-        /// </summary>
-        /// <param name="arr"></param>
-        /// <returns></returns>
-        private static int GetMax(int[] arr)
-        {
-            int mx = arr[0];
-            for (int i = 1; i < arr.Length; i++)
-            {
-                if (arr[i] > mx)
-                    mx = arr[i];
-            }
-
-            return mx;
-        }
-
-        /// <summary>
-        /// A function to do counting sort of arr[] according to 
-        /// the digit represented by exp. (e.g. 300 is represented by 100)
-        /// </summary>
-        /// <param name="arr"></param>
-        /// <param name="exp"></param>
-        private static void CountSort(int[] arr, int exp)
-        {
-            int[] output = new int[arr.Length]; // output array 
-            int i;
-            int[] count = new int[10];
-            Array.Fill(count, 0);
-
-            // Store count of occurrences in count[] 
-            for (i = 0; i < arr.Length; i++)
-                count[(arr[i] / exp) % 10]++;
-
-            // Change count[i] so that count[i] now contains 
-            // actual position of this digit in output[] 
-            for (i = 1; i < 10; i++)
-                count[i] += count[i - 1];
-
-            // Build the output array 
-            for (i = arr.Length - 1; i >= 0; i--)
-            {
-                output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-                count[(arr[i] / exp) % 10]--;
-            }
-
-            // Copy the output array to arr[], so that arr[] now 
-            // contains sorted numbers according to current digit 
-            for (i = 0; i < arr.Length; i++)
-                arr[i] = output[i];
-        }
-
-        /// <summary>
-        /// The main function to that sorts array using Radix Sort 
-        /// </summary>
-        /// <param name="arr"></param>
-        /// <returns></returns>
-        public static int[] Sort(int[] arr)
+        public static long[] SortL(long[] arr)
         {
             if (arr == null || arr.Length == 0)
                 return arr;
 
-            // Find the maximum number to know number of digits 
-            int m = GetMax(arr);
-
-            // Do counting sort for every digit. Note that instead 
-            // of passing digit number, exp is passed. exp is 10^i 
-            // where i is current digit number 
-            for (int exp = 1; m / exp > 0; exp *= 10)
-                CountSort(arr, exp);
+            int i, j;
+            var tmp = new long[arr.Length];
+            for (int shift = sizeof(long)*8 - 1; shift > -1; --shift)
+            {
+                j = 0;
+                for (i = 0; i < arr.Length; ++i)
+                {
+                    var move = (arr[i] << shift) >= 0;
+                    if (shift == 0 ? !move : move)
+                        arr[i - j] = arr[i];
+                    else
+                        tmp[j++] = arr[i];
+                }
+                Array.Copy(tmp, 0, arr, arr.Length - j, j);
+            }
 
             return arr;
-        }
+        }        
     }
 }
