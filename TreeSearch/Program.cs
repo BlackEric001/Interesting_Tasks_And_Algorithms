8920ccb904dc9a5fb675548911637a13c5c48e39
BlackEric001 <ericblack@mail.ru>
2020-02-26 15:57:56 +0300

    Добавил проект для примера работы с деревом

---
+++ TreeSearch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TreeSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Получение данных с n-го уровня дерева");
+
+            ;
+
+            Console.WriteLine("The End");
+            Console.ReadLine();
+        }
+    }
+}
